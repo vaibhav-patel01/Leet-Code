139394805bcfd43078918e870b81d923a9643f87
Vaibhav Patel <vaibhav.patel.7638@gmail.com>
2026-06-26 19:29:53 +0530

    Time: 1 ms (78.11%), Space: 41.5 MB (99.91%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,38 +1,21 @@
 class Solution {
     public boolean isHappy(int n) {
-        if(n==1 || n==7) return true;
-        else if(n<10) return false;
-        HashSet<Integer> set = new HashSet<>();
-        int ans = 0;
-        while(n  > 0){
-            int temp = n % 10;
-            ans += (temp * temp);
-            n /= 10;
-            if(n == 0){
-                if(ans == 1) return true;
-                if(set.contains(ans)){
-                    return false;
-                }
-                else set.add(ans);
-                n = ans;
-                ans = 0;
-            }
-        }
+        int slow = n;
+        int fast = n;
+        do{
+            slow = nextNumber(slow);                       // slow fast pointer approach
+            fast = nextNumber(nextNumber(fast));
+        }while(slow != fast);
+        if(slow == 1) return true ;
         return false;
     }
+    private int nextNumber(int n){
+        int ans = 0;
+        while(n > 0){
+            int temp = n%10;
+            ans += temp * temp;
+            n /= 10;
+        }
+        return ans;
+    } 
 }
-// class Solution {
-//     public boolean isHappy(int n) {
-//         if(n==1 || n==7) return true;
-//         else if(n<10) return false;
-//         else{
-//             int sum=0;
-//             while(n>0){
-//                 int temp=n%10;
-//                 sum+= temp*temp;
-//                 n=n/10;
-//             }
-//             return isHappy(sum);
-//         }
-//     }
-// }
