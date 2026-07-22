76be16bb1a06940c0733090f7aafa532dd4b53a2
Vaibhav Patel <vaibhav.patel.7638@gmail.com>
2026-07-22 17:21:58 +0530

    Time: 6 ms (96.34%), Space: 46 MB (75.34%) - LeetHub

--- 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.java
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.java
@@ -1,12 +1,19 @@
 class Solution {
     public int[] maxSubsequence(int[] nums, int k) {
         PriorityQueue<int[]> minHeap = new PriorityQueue<>((n1, n2) -> Integer.compare(n1[1] , n2[1]));
-        for (int i = 0; i < nums.length; i++) {
-        minHeap.offer(new int[]{i, nums[i]});
-        if (minHeap.size() > k) {
-            minHeap.poll();
+        for (int i = 0 ; i < k ;i++){
+            minHeap.add(
+                new int[]{i, nums[i]}
+            );
         }
-    }
+        for( int i = k ; i < nums.length; i++ ){
+            if(nums[i] > minHeap.peek()[1]){
+                minHeap.remove();
+                minHeap.add(
+                    new int[]{i, nums[i]}
+                );
+            }
+        }
     PriorityQueue<int[]> indexHeap = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
     indexHeap.addAll(minHeap);
     
