82aa92744e3791975ab84ad62df20d9f5ff26896
Vaibhav Patel <vaibhav.patel.7638@gmail.com>
2026-09-09 11:09:41 +0530

    Time: 1 ms (84.64%), Space: 42.8 MB (58.15%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -28,3 +28,42 @@ class Solution {
         return currStr.toString();
     }
 }
+
+// class Solution {
+//     public String decodeString(String s) {
+//         Deque<String> stack = new ArrayDeque<>();
+//         int n = s.length(); 
+//         for (int i = 0 ;i < n ; i++){
+//             char c = s.charAt(i); 
+//             if(c == ']'){
+//                 StringBuilder sb = new StringBuilder();
+//                 StringBuilder num = new StringBuilder();
+//                 while(!stack.isEmpty() && !stack.peek().equals("[")){
+//                     sb.append(stack.pop());
+//                 }
+//                 sb.reverse();
+//                 if (!stack.isEmpty() && stack.peek().equals("[")) {
+//                     stack.pop();
+//                 }
+//                 while(!stack.isEmpty() && Character.isDigit(stack.peek().charAt(0)) ){
+//                     num.append(stack.pop());
+//                 }
+//                 num.reverse();
+//                 int number = Integer.parseInt(num.toString());
+//                 StringBuilder repeated = new StringBuilder();
+//                 for (int k = 0; k < number; k++) {
+//                     repeated.append(sb);
+//                 }
+//                 stack.push(repeated.toString());
+//             }
+//             else{
+//                 stack.push(String.valueOf(c));
+//             }
+//         }
+//         StringBuilder result = new StringBuilder();
+//         while (!stack.isEmpty()) {
+//     result.append(stack.pollLast()); 
+// }
+//         return result.toString();
+//     }
+// }
